ca293053c95c16cdb64feceba40f4f0d5325ccf2
FanisX <ya.faniska@gmail.com>
2017-06-18 14:01:27 +0300

    Домашнее задание к лекции 1.5

---
+++ PY5-1.5/script.py
@@ -0,0 +1,499 @@
+students = [
+    {
+        'id': 1,
+        'first_name': 'Василий',
+        'last_name': 'Зяблицев',
+        'sex': 'm',
+        'experience': True
+    },
+    {
+        'id': 2,
+        'first_name': 'Дмитрий',
+        'last_name': 'Ганин',
+        'sex': 'm',
+        'experience': False
+    },
+    {
+        'id': 3,
+        'first_name': 'Екатерина',
+        'last_name': 'Гущенко',
+        'sex': 'w',
+        'experience': True
+    },
+    {
+        'id': 4,
+        'first_name': 'Елена',
+        'last_name': 'Белова',
+        'sex': 'w',
+        'experience': False
+    },
+    {
+        'id': 5,
+        'first_name': 'Денис',
+        'last_name': 'Торин',
+        'sex': 'm',
+        'experience': True
+    },
+    {
+        'id': 6,
+        'first_name': 'Дмитрий',
+        'last_name': 'Шибков',
+        'sex': 'm',
+        'experience': False
+    },
+    {
+        'id': 7,
+        'first_name': 'Мария',
+        'last_name': 'Белашова',
+        'sex': 'w',
+        'experience': True
+    },
+    {
+        'id': 8,
+        'first_name': 'Евгений',
+        'last_name': 'Градобаев',
+        'sex': 'm',
+        'experience': False
+    },
+    {
+        'id': 9,
+        'first_name': 'Александр',
+        'last_name': 'Сидоров',
+        'sex': 'm',
+        'experience': True
+    },
+    {
+        'id': 10,
+        'first_name': 'Юлия',
+        'last_name': 'Хайруллина',
+        'sex': 'w',
+        'experience': False
+    }
+]
+
+assessment = [
+    {
+        'id': 1,
+        'student_id': 1,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 2,
+        'student_id': 1,
+        'type': 'home_work',
+        'value': 5
+    },
+    {
+        'id': 3,
+        'student_id': 1,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 4,
+        'student_id': 1,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 5,
+        'student_id': 1,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 6,
+        'student_id': 1,
+        'type': 'exam',
+        'value': 10
+    },
+    {
+        'id': 7,
+        'student_id': 2,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 8,
+        'student_id': 2,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 9,
+        'student_id': 2,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 10,
+        'student_id': 2,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 11,
+        'student_id': 2,
+        'type': 'home_work',
+        'value': 4
+    },
+    {
+        'id': 12,
+        'student_id': 2,
+        'type': 'exam',
+        'value': 9
+    },
+    {
+        'id': 13,
+        'student_id': 3,
+        'type': 'exam',
+        'value': 8
+    },
+    {
+        'id': 14,
+        'student_id': 3,
+        'type': 'home_work',
+        'value': 5
+    },
+    {
+        'id': 15,
+        'student_id': 3,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 16,
+        'student_id': 3,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 17,
+        'student_id': 3,
+        'type': 'home_work',
+        'value': 4
+    },
+    {
+        'id': 18,
+        'student_id': 3,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 19,
+        'student_id': 4,
+        'type': 'exam',
+        'value': 9
+    },
+    {
+        'id': 20,
+        'student_id': 4,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 21,
+        'student_id': 4,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 22,
+        'student_id': 4,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 23,
+        'student_id': 4,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 24,
+        'student_id': 4,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 25,
+        'student_id': 5,
+        'type': 'exam',
+        'value': 9
+    },
+    {
+        'id': 26,
+        'student_id': 5,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 27,
+        'student_id': 5,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 28,
+        'student_id': 5,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 29,
+        'student_id': 5,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 30,
+        'student_id': 5,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 31,
+        'student_id': 6,
+        'type': 'exam',
+        'value': 10
+    },
+    {
+        'id': 32,
+        'student_id': 6,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 33,
+        'student_id': 6,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 34,
+        'student_id': 6,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 35,
+        'student_id': 6,
+        'type': 'home_work',
+        'value': 10
+    },
+    {
+        'id': 36,
+        'student_id': 6,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 37,
+        'student_id': 7,
+        'type': 'exam',
+        'value': 10
+    },
+    {
+        'id': 38,
+        'student_id': 7,
+        'type': 'home_work',
+        'value': 10
+    },
+    {
+        'id': 39,
+        'student_id': 7,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 40,
+        'student_id': 7,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 41,
+        'student_id': 7,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 42,
+        'student_id': 7,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 43,
+        'student_id': 8,
+        'type': 'exam',
+        'value': 8
+    },
+    {
+        'id': 44,
+        'student_id': 8,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 45,
+        'student_id': 8,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 46,
+        'student_id': 8,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 47,
+        'student_id': 8,
+        'type': 'home_work',
+        'value': 10
+    },
+    {
+        'id': 48,
+        'student_id': 8,
+        'type': 'home_work',
+        'value': 5
+    },
+    {
+        'id': 49,
+        'student_id': 9,
+        'type': 'exam',
+        'value': 9
+    },
+    {
+        'id': 50,
+        'student_id': 9,
+        'type': 'home_work',
+        'value': 8
+    },
+    {
+        'id': 51,
+        'student_id': 9,
+        'type': 'home_work',
+        'value': 10
+    },
+    {
+        'id': 52,
+        'student_id': 9,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 53,
+        'student_id': 9,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 54,
+        'student_id': 9,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 55,
+        'student_id': 10,
+        'type': 'exam',
+        'value': 9
+    },
+    {
+        'id': 56,
+        'student_id': 10,
+        'type': 'home_work',
+        'value': 9
+    },
+    {
+        'id': 57,
+        'student_id': 10,
+        'type': 'home_work',
+        'value': 7
+    },
+    {
+        'id': 58,
+        'student_id': 10,
+        'type': 'home_work',
+        'value': 6
+    },
+    {
+        'id': 59,
+        'student_id': 10,
+        'type': 'home_work',
+        'value': 5
+    },
+    {
+        'id': 60,
+        'student_id': 10,
+        'type': 'home_work',
+        'value': 7
+    }
+]
+
+
+def get_student(student_id):
+    return next((student for student in students if student['id'] == student_id), None)
+
+
+def get_avg_grade(work_type, student_id=0, sex=None, experience=None):
+    grades = []
+    for grade in assessment:
+        if grade['type'] != work_type:
+            continue
+
+        # Если указан студент, то среднюю оценку вычисляем только для него
+        if student_id and grade['student_id'] != student_id:
+            continue
+
+        # Получаем данные о студенте
+        student = get_student(grade['student_id'])
+
+        # Если указан пол и он не совпадает, пропускаем
+        if sex is not None and student['sex'] != sex:
+            continue
+        # Если указан опыт и он не совпадает, пропускаем
+        if experience is not None and student['experience'] != experience:
+            continue
+
+        grades.append(grade['value'])
+    return sum(grades) / len(grades) if grades else None
+
+
+def print_best_students():
+    max_integral = 0
+    for student in students:
+        home_work_value = get_avg_grade('home_work', student_id=student['id']) * 0.6
+        exam_value = get_avg_grade('exam', student_id=student['id']) * 0.4
+        student['integral'] = home_work_value + exam_value
+        if max_integral < student['integral']:
+            max_integral = student['integral']
+
+    best_students = []
+    for student in students:
+        if student['integral'] == max_integral:
+            student_name = student['first_name'] + ' ' + student['last_name']
+            best_students.append(student_name)
+
+    title = 'Лучший студент' if len(best_students) == 1 else 'Лучшие студенты'
+    best_students_names = ', '.join(best_students)
+    print('{}: {} с интегральной оценкой {}'.format(title, best_students_names, max_integral))
+
+
+print('Средняя оценка за домашние задания по группе:', get_avg_grade('home_work'))
+print('Средняя оценка за экзамен: ', get_avg_grade('exam'))
+print('----------')
+print('Средняя оценка за домашние задания у мужчин:', get_avg_grade('home_work', sex='m'))
+print('Средняя оценка за экзамен у мужчин:', get_avg_grade('exam', sex='m'))
+print('Средняя оценка за домашние задания у женщин:', get_avg_grade('home_work', sex='w'))
+print('Средняя оценка за экзамен у женщин:', get_avg_grade('exam', sex='w'))
+print('----------')
+print('Средняя оценка за домашние задания у студентов с опытом:', get_avg_grade('home_work', experience=True))
+print('Средняя оценка за экзамен у студентов с опытом:', get_avg_grade('exam', experience=True))
+print('Средняя оценка за домашние задания у студентов без опыта:', get_avg_grade('home_work', experience=False))
+print('Средняя оценка за экзамен у студентов без опыта:', get_avg_grade('exam', experience=False))
+print('----------')
+print_best_students()
